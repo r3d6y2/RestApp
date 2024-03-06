e12b334c09b14739e7e8447f65c16a0510791bb3
OleksiiVasiutaEPAM <oleksii_vasiuta@epam.com>
2024-03-06 08:55:00 +0100

    Initial

---
+++ RestApp/RestApp.Test/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RestApp.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
